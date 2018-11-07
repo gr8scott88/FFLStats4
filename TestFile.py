e5fdc958d0c77fd2d4a2dd30c0b18cb7fb84eb27
gr8scott88 <gr8scott88@gmail.com>
2018-11-06 20:47:44 -0500

    Week 9 Info

--- TestFile.py
+++ TestFile.py
@@ -4,11 +4,11 @@ import DataManager
 nfc_id = 729457
 nfc_data = DataManager.DataManager()
 NFC = League.League(nfc_id, nfc_data)
-NFC.load_all_data_points(8)
+NFC.load_all_data_points(9)
 nfc_data.export_complete_team_frame(nfc_id)
 
 afc_id = 910981
 afc_data = DataManager.DataManager()
 AFC = League.League(afc_id, afc_data)
-AFC.load_all_data_points(8)
+AFC.load_all_data_points(9)
 afc_data.export_complete_team_frame(afc_id)
