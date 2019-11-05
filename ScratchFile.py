41945e361f99e0b76faf7ead95f989922ddf2b6e
Scott <gr8scott88@gmail.com>
2019-11-04 21:30:07 -0500

    Quick cleanup

--- ScratchFile.py
+++ ScratchFile.py
@@ -25,23 +25,3 @@ NFC.load_team_scores_through_week(load_thru_week)
 
 AFC_vis.plot_cum_real_vs_proj_by_week()
 NFC_vis.plot_cum_real_vs_proj_by_week()
-
-merged = pd.merge(AFC.score_info, AFC.league_info, on='TeamID', how='left')
-merged['Delta'] = merged['RealScore']-merged['ProjScore']
-grouped = merged.groupby('TeamName')
-
-for name, group in grouped:
-    print(name)
-    print(grouped)
-
-fig, ax = plt.subplots(figsize=(15,7))
-
-grouped.plot(x='Week', y='Delta', ax=ax)
-plt.show()
-
-
-# sdf = pd.DataFrame(data=scores)
-# sorted_scores = sdf.sort_values('0')
-
-s = helper.get_team_soup_by_week(AFC_id, 4, 4)
-handler = PandasDataHandler()
